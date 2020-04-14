c3d48f6c43726755e72ad67519ce67ed8c4cbc55
joshand@cisco.com <joshand@cisco.com>
2020-04-14 16:03:39 -0500

    add better status for tasks

--- adaptive_policy_sync/tasks.py
+++ adaptive_policy_sync/tasks.py
@@ -10,16 +10,28 @@ def run_tasks():
     try:
         import scripts.clean_tasks
         scripts.clean_tasks.run()
+    except:
+        print("#### Exception starting scheduled job: clean_tasks")
 
+    try:
         import scripts.dashboard_monitor
         scripts.dashboard_monitor.run()
+    except:
+        print("#### Exception starting scheduled job: dashboard_monitor")
 
+    try:
         import scripts.dashboard_webhook
         scripts.dashboard_webhook.run()
+    except:
+        print("#### Exception starting scheduled job: dashboard_webhook")
 
+    try:
         import scripts.ise_monitor
         scripts.ise_monitor.run()
+    except:
+        print("#### Exception starting scheduled job: ise_monitor")
 
+    try:
         loop = asyncio.new_event_loop()
         import scripts.pxgrid_websocket
         th = threading.Thread(target=scripts.pxgrid_websocket.start_background_loop, args=(loop,))
@@ -27,4 +39,4 @@ def run_tasks():
         log = []
         scripts.pxgrid_websocket.sync_pxgrid(loop, log)
     except:
-        print("#### Exception starting scheduled jobs")
+        print("#### Exception starting scheduled job: sync_pxgrid")
