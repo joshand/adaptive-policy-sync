5165309784309967c3f9cd2ae1c9ad56fa92474d
joshand@cisco.com <joshand@cisco.com>
2020-04-24 17:22:44 -0500

    trying to get paths to display on start-server

--- start-server.sh
+++ start-server.sh
@@ -7,7 +7,7 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] &&
     (cd adaptive_policy_sync; python manage.py runscript import_token --script-args $DJANGO_SUPERUSER_USERNAME $DJANGO_SUPERUSER_APIKEY)
 fi
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$SIMULATED_ENVIRONMENT_URL" ] ; then
-    (cd adaptive_policy_sync; python manage.py runscript dashboard_simulator --script-args 10 10 10 10; python manage.py runscript ise_ers_simulator --script-args 10 10 10; echo Use '$SIMULATED_ENVIRONMENT_URL/meraki/api/v1' for Meraki Dashboard\nUse '$SIMULATED_ENVIRONMENT_URL' for Cisco ISE)
+    (cd adaptive_policy_sync; python manage.py runscript dashboard_simulator --script-args 10 10 10 10; python manage.py runscript ise_ers_simulator --script-args 10 10 10; echo Use $SIMULATED_ENVIRONMENT_URL/meraki/api/v1 for Meraki Dashboard;echo Use $SIMULATED_ENVIRONMENT_URL for Cisco ISE)
 fi
 (cd adaptive_policy_sync; gunicorn adaptive_policy_sync.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
