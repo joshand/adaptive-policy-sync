5a4203046f3e78badf5c3d5c55328788a75ae36c
joshand@cisco.com <joshand@cisco.com>
2020-04-27 19:50:55 -0500

    add more instructions to start-server for demo run

--- start-server.sh
+++ start-server.sh
@@ -7,7 +7,15 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] &&
     (cd adaptive_policy_sync; python manage.py runscript import_token --script-args $DJANGO_SUPERUSER_USERNAME $DJANGO_SUPERUSER_APIKEY)
 fi
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$SIMULATED_ENVIRONMENT_URL" ] ; then
-    (cd adaptive_policy_sync; python manage.py runscript dashboard_simulator --script-args 10 10 10 10; python manage.py runscript ise_ers_simulator --script-args 10 10 10; echo ====================================================; echo Use $SIMULATED_ENVIRONMENT_URL/meraki/api/v1 for Meraki Dashboard; echo Use $SIMULATED_ENVIRONMENT_URL/ise for Cisco ISE; echo ====================================================; cd scripts; chown www-data:www-data *.json)
+    (cd adaptive_policy_sync; python manage.py runscript dashboard_simulator --script-args 10 5 5 5; python manage.py runscript ise_ers_simulator --script-args 5 5 5; cd scripts; chown www-data:www-data *.json)
+    echo ====================================================
+    echo For Cisco ISE, use the following settings:
+    echo 1- IP Address: $SIMULATED_ENVIRONMENT_URL/ise
+    echo 2- Username/Password:(not required; enter anything)
+    echo For Meraki Dashboard, use the following settings:
+    echo 1- Path: $SIMULATED_ENVIRONMENT_URL/meraki/api/v1
+    echo 2- API Key:(not required; enter anything)
+    echo ====================================================
 fi
 (cd adaptive_policy_sync; gunicorn adaptive_policy_sync.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 --preload) &
 #(cd adaptive_policy_sync; python manage.py runscript dashboard_monitor) &
