acf02b90fddbee6f5a07014050023ac136aee368
joshand@cisco.com <joshand@cisco.com>
2020-05-13 19:20:15 -0500

    disable tasks when setting SKIPTASKS=true in env

--- adaptive_policy_sync/tasks.py
+++ adaptive_policy_sync/tasks.py
@@ -1,8 +1,13 @@
 import sys
 import os
-if 'runscript' not in sys.argv and os.getenv('SKIPTASKS', '').upper() != "TRUE":
+from django_apscheduler.models import DjangoJob
+
+if ('runscript' not in sys.argv) and (os.getenv('SKIPTASKS', '').upper() != "TRUE"):
     import scripts.dashboard_monitor
     import scripts.ise_monitor
     import scripts.clean_tasks
     import scripts.pxgrid_websocket
     import scripts.dashboard_webhook   # noqa: F401
+else:
+    for j in DjangoJob.objects.all():
+        j.delete()
