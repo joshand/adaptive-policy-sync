ce6d31d1cc922ea588aca4aa0b0e20cb93a5bea3
joshand@cisco.com <joshand@cisco.com>
2020-05-26 14:41:10 -0500

    add coverage report

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    test_fn = item.obj
+    docstring = getattr(test_fn, '__doc__')
+    if docstring:
+        report.nodeid = docstring
+
+
+@pytest.fixture(scope='session')
+def django_db_modify_db_settings():
+    pass
