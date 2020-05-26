f253531e17be8dcc59029182ad4c37415895c91f
joshand@cisco.com <joshand@cisco.com>
2020-05-26 14:43:06 -0500

    unit tests

--- conftest.py
+++ conftest.py
@@ -1,17 +1,20 @@
 import pytest
+import sys
 
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
+    if "-vv" in sys.argv:
+        outcome = yield
+        report = outcome.get_result()
 
-    test_fn = item.obj
-    docstring = getattr(test_fn, '__doc__')
-    if docstring:
-        report.nodeid = docstring
+        test_fn = item.obj
+        docstring = getattr(test_fn, '__doc__')
+        if docstring:
+            report.nodeid = docstring
+    else:
+        outcome = yield
 
-
-@pytest.fixture(scope='session')
-def django_db_modify_db_settings():
-    pass
+# @pytest.fixture(scope='session')
+# def django_db_modify_db_settings():
+#     pass
